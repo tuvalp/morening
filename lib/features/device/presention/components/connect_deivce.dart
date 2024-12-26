73d918fcd5dedb36a229c0939729bea25c6c81fb
Mac Computer <maccomputer@Macs-MacBook-Pro.local>
2024-12-26 15:47:19 +0200

    ssid

--- lib/features/device/presention/components/connect_deivce.dart
+++ lib/features/device/presention/components/connect_deivce.dart
@@ -61,7 +61,7 @@ class _ConnectDeivceSheetState extends State<ConnectDeivceSheet> {
       bool isConnected = await WiFiForIoTPlugin.connect(
         "morening",
         password: "12345678",
-        security: NetworkSecurity.NONE,
+        security: NetworkSecurity.WEP,
         joinOnce: true,
       );
 
