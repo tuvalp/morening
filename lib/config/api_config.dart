d0f6bad56c0ed6452c6f9c541d8c5aab7f360b2e
Mac Computer <maccomputer@Macs-MacBook-Pro.local>
2025-01-21 20:31:23 +0200

    fiox

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
-  static const String baseUrl = "http://16.171.54.94:3250/api/v1/";
-  //"http://morenign-prod-dev-851050842.eu-north-1.elb.amazonaws.com:3250/api/v1/";
+  static const String baseUrl = //"http://16.171.54.94:3250/api/v1/";
+      "http://morenign-prod-dev-851050842.eu-north-1.elb.amazonaws.com:3250/api/v1/";
   static const String deviceUrl = "http://10.42.0.1:5000/";
 
   static const Map<String, String> defaultHeaders = {
